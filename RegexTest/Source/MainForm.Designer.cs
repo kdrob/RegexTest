70cc301ede8c897734e7aa8b8bc9a65f3cf55ca4
kdrob <kurt.robideau@honeywell.com>
2014-10-20 13:39:15 -0500

    Initial

---
+++ RegexTest/Source/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace RegexTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.FileNameTextBox = new System.Windows.Forms.TextBox();
+            this.SelectedLineTextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StatusTextBox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(12, 12);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 46);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // FileNameTextBox
+            // 
+            this.FileNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileNameTextBox.Location = new System.Drawing.Point(109, 38);
+            this.FileNameTextBox.Name = "FileNameTextBox";
+            this.FileNameTextBox.Size = new System.Drawing.Size(411, 20);
+            this.FileNameTextBox.TabIndex = 1;
+            // 
+            // SelectedLineTextBox
+            // 
+            this.SelectedLineTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.SelectedLineTextBox.Location = new System.Drawing.Point(12, 104);
+            this.SelectedLineTextBox.Name = "SelectedLineTextBox";
+            this.SelectedLineTextBox.Size = new System.Drawing.Size(508, 41);
+            this.SelectedLineTextBox.TabIndex = 2;
+            this.SelectedLineTextBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SelectedLine:";
+            // 
+            // StatusTextBox
+            // 
+            this.StatusTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusTextBox.Location = new System.Drawing.Point(12, 177);
+            this.StatusTextBox.Name = "StatusTextBox";
+            this.StatusTextBox.Size = new System.Drawing.Size(508, 58);
+            this.StatusTextBox.TabIndex = 4;
+            this.StatusTextBox.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Status:";
+            // 
+            // NextButton
+            // 
+            this.NextButton.Enabled = false;
+            this.NextButton.Location = new System.Drawing.Point(295, 261);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(75, 59);
+            this.NextButton.TabIndex = 6;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 332);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.StatusTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SelectedLineTextBox);
+            this.Controls.Add(this.FileNameTextBox);
+            this.Controls.Add(this.LoadButton);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.TextBox FileNameTextBox;
+        private System.Windows.Forms.RichTextBox SelectedLineTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox StatusTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button NextButton;
+    }
+}
+
---
+++ RegexTest/Source/MainForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RegexTest
+{
+    public partial class MainForm : Form
+    {
+        StreamReader sr;
+        Regex r;
+
+        private List<string> THMsgTypeStrings = new List<string>()
+        {
+            "1001", 
+            "1002",
+            "1003",
+            "1004",
+            "1011",
+            "9001",
+            "9002",
+            "9003",
+            "9004",
+            "9011",
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileNameTextBox.Text = openFileDialog1.FileName;
+//                LogTextBox.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                NextButton.Enabled = true;
+                LoadButton.Enabled = false;
+                sr = new StreamReader(FileNameTextBox.Text);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < THMsgTypeStrings.Count(); i++)
+                {
+                    sb.Append(THMsgTypeStrings[i]);
+                    sb.Append("|");
+                }
+                //                        var pattern = "( |:|\\n)" + sb.ToString().TrimEnd('|') + "([^|\\|]+)";
+                string pattern = "(?<![A-F0-9])(" + sb.ToString().TrimEnd('|') + ")";
+                r = new Regex(pattern);
+
+            }
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            string line = sr.ReadLine();
+            if (line != null){
+                SelectedLineTextBox.Text = line;
+            }
+            Match m = r.Match(line);
+            if (m.Success)
+            {
+                string matchVal = m.Value;
+                int indexOf = line.IndexOf(matchVal);
+                Match fullMatch = Regex.Match(line.Substring(indexOf), @"\w+\b");
+                if (fullMatch.Length > 4)
+                    StatusTextBox.Text = "Match: " + matchVal + "  " + fullMatch.Value;
+                else
+                    StatusTextBox.Clear();
+            }
+            else
+            {
+                StatusTextBox.Clear();            
+            }
+        }
+
+
+    }
+}
